f8accdc31da4b9484085ae35ce1113d20ef7b28e
Amfishers <378437235@qq.com>
2022-05-12 22:01:38 +0800

    feat: 分片上传

--- exercise/demo/splice-file/index.js
+++ exercise/demo/splice-file/index.js
@@ -2,20 +2,31 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const server = require('koa-static')
 const multiparty = require('multiparty')
+const bodyParser = require('koa-bodyparser')
 
 const path = require('path')
 
 const app = new Koa()
 const router = new Router()
 
+
+app.use(bodyParser())
+
 app.use(router.routes())
 app.use(server(path.join(__dirname, '/public')))
+
+router.post('/pageUnload', async ctx => {
+    let data = ctx.request.body
+    console.log("注册页提交的数据是：", data) //{"userName":"admin","password":"123"}
+    ctx.body = data
+})
 
 router.post('/upload', async (ctx) => {
     const form = new multiparty.Form({
         uploadDir: 'temp'
     })
-    console.log('ctx', ctx)
+
+    console.log()
 
     form.parse(ctx.req)
 
@@ -25,6 +36,8 @@ router.post('/upload', async (ctx) => {
 
     ctx.response.body = 'uploaded successed'
 })
+
+
 
 app.listen(3020, () => {
     console.log('服务器启动，监听 3020 端口')
