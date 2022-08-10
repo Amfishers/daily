da1ed401c95a5dccddc05dce7b5671e755107b24
Amfishers <378437235@qq.com>
2022-08-10 21:09:07 +0800

    feat: typescript 常见类型

---
+++ course/learn-ts/normal-type/type.ts
@@ -0,0 +1,14 @@
+const a:number = 1
+const b : string = '1'
+const c : boolean = true
+
+// 下面两个都是表示一个意思
+const d : Array<number> = [1,2,3]
+const e : string[] = ['12', '2']
+
+// 函数参数以及返回值类型定义
+function getFavoriteNumber (num: number) : number {
+    return 1 + num
+}
+
+
