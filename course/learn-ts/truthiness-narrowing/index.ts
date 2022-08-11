ab16cbbf04431930ff6616e9e455e327275df119
Amfishers <378437235@qq.com>
2022-08-11 21:25:12 +0800

    feat: typscript 穷尽检查

--- course/learn-ts/truthiness-narrowing/index.ts
+++ course/learn-ts/truthiness-narrowing/index.ts
@@ -40,7 +40,14 @@ interface Square {
     kind: 'square';
     sideLength: number
 }
-type Shape1 = Circle | Square
+interface Triangle {
+    kind: 'triangle',
+    sideLength: number
+}
+type Shape1 = Circle | Square | Triangle
+
+// 穷尽检查（Exhaustiveness checking）
+// never
 
 const getAre1 = (shape: Shape1) : number => {
     switch (shape.kind) {
@@ -49,6 +56,14 @@ const getAre1 = (shape: Shape1) : number => {
         
         case 'square':
             return shape.sideLength ** 2
+
+        default:
+            const _exhuastCheck: never = shape
+            // Type 'Triangle' is not assignable to type 'never'.
+            return _exhuastCheck
     }
     
 }
+// 这一点没明白？：
+// 因为 TypeScript 的收窄特性，执行到 default 的时候，类型被收窄为 Triangle，但因为任何类型都不能赋值给 never 类型，这就会产生一个编译错误。通过这种方式，你就可以确保 getArea 函数总是穷尽了所有 shape 的可能性。
+console.log(getAre1({kind: 'triangle', sideLength: 2}))
