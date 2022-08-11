003cc02c39e56390d35eaf911394b300675be3c1
Amfishers <378437235@qq.com>
2022-08-11 21:04:33 +0800

    feat: typescript 可辨别联合

--- course/learn-ts/truthiness-narrowing/index.ts
+++ course/learn-ts/truthiness-narrowing/index.ts
@@ -14,3 +14,41 @@
       // do nothing
     }
   }
+
+/**
+ * 可辨别联合
+ */
+
+ interface Shape {
+    kind: "circle" | "square"
+    radius?: number
+    sideLength?: number
+}
+
+function getArea (shape: Shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2
+        // Object is possibly 'undefined'.
+    }
+}
+// 可以改成
+interface Circle {
+    kind: 'circle',
+    radius: number
+}
+interface Square {
+    kind: 'square';
+    sideLength: number
+}
+type Shape1 = Circle | Square
+
+const getAre1 = (shape: Shape1) : number => {
+    switch (shape.kind) {
+        case 'circle':
+            return Math.PI * shape.radius ** 2
+        
+        case 'square':
+            return shape.sideLength ** 2
+    }
+    
+}
